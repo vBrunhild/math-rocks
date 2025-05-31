ce575d36fcbe647de615f4943e6326d683b43c91
BetaNeumann <brunomoretti100@gmail.com>
2025-05-30 23:31:28 -0400

    Test(error): created unit tests for parser error convenience methods

--- src/parser/error.rs
+++ src/parser/error.rs
@@ -64,3 +64,57 @@ impl ParserError {
 }
 
 pub type Result<T> = std::result::Result<T, ParserError>;
+
+#[cfg(test)]
+mod test {
+    use super::ParserError;
+    use proptest::prelude::*;
+
+    fn err_strategy() -> impl Strategy<Value = ParserError> {
+        let base_errors = prop_oneof![
+            Just(ParserError::Token('a')),
+            Just(ParserError::ZeroValue),
+            Just(ParserError::Identifier("id".into())),
+            Just(ParserError::RollExpr("expr".into())),
+            Just(ParserError::Empty),
+            Just(ParserError::UnclosedParenthesis),
+            Just(ParserError::UnexpectedDiceExpression("dice".into())),
+            Just(ParserError::UnexpectedPrefix("prefix".into())),
+            Just(ParserError::UnexpectedInfix("infix".into())),
+        ];
+        
+        base_errors.prop_recursive(1, 16, 1, |inner| {
+                prop_oneof![
+                    inner.clone(),
+                    (0usize..=100, inner).prop_map(|(position, err)| err.at_pos(position))
+                ]
+            }
+        )
+    }
+
+    proptest! {
+        #[test]
+        fn test_parser_error_methods(err in err_strategy(), pos in 0usize..=100) {
+            match &err {
+                ParserError::AtPosition(_, boxed_err) => {
+                    prop_assert!(std::ptr::eq(err.err(), boxed_err.as_ref()));
+                    prop_assert!(err.pos().is_some());
+                },
+                other => {
+                    prop_assert!(std::ptr::eq(err.err(), other));
+                    prop_assert!(other.pos().is_none());
+                }
+            };
+
+            let err_at_pos = err.clone().at_pos(pos);
+
+            match err {
+                ParserError::AtPosition(_, _) => prop_assert_eq!(err, err_at_pos),
+                _ => prop_assert!(matches!(
+                    err_at_pos,
+                    ParserError::AtPosition(err_pos, _) if err_pos == pos
+                ))
+            };
+        }
+    }
+}
